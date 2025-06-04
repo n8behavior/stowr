108d0539ff7192ce6d9f12bfffcfac5b32e4d3f6
Mike Sandman <mike@thesandmans.com>
2025-06-03 21:44:59 -0400

    add setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apt install \
+  build-essential \
+  curl \
+  file \
+  libatk1.0-dev \
+  libayatana-appindicator3-dev \
+  libcairo2-dev \
+  libgdk-pixbuf2.0-dev \
+  libgdk3.0-cil \
+  libjavascriptcoregtk-4.1-dev \
+  libpango1.0-dev \
+  librsvg2-dev \
+  libsoup-3.0-dev \
+  libssl-dev \
+  libwebkit2gtk-4.1-dev \
+  libxdo-dev \
+  wget
